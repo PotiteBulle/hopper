0a124555689bbbc45e85bbbd98c6b676a2dc63b2
Potite_Bulle <135218431+bashx00@users.noreply.github.com>
2024-03-29 21:21:24 +0100

    Update + Inclusive Code

--- hopper.js
+++ hopper.js
@@ -9,21 +9,21 @@ const client = new Discord.Client();
 client.once('ready', () => {
     console.log('Bot is ready.');
 
-    // Lire la liste des utilisateurs à bannir depuis le fichier txt
+    // Lire la liste des utilisateurices à bannir depuis le fichier txt
     fs.readFile('./bannissements/usersToBan.txt', 'utf8', (err, data) => {
         if (err) {
             console.error('Failed to read usersToBan.txt:', err);
             return;
         }
 
-        // Séparer les identifiants des utilisateurs en utilisant des sauts de ligne comme délimiteurs
+        // Séparer les identifiants des utilisateurices en utilisant des sauts de ligne comme délimiteurs
         const userIds = data.trim().split('\n');
 
         // Parcourir chaque serveur Discord où le bot est présent
         client.guilds.cache.forEach(guild => {
             // Parcourir chaque identifiant d'utilisateur
             userIds.forEach(userId => {
-                // Bannir l'utilisateur du serveur Discord avec la raison spécifiée
+                // Bannir les utilisateurices du serveur Discord avec la raison spécifiée
                 guild.members.ban(userId.trim(), { reason: 'Demandes et participations aux partages de contenus interdits.' })
                     .then(user => console.log(`Banned user ${user.id} from server ${guild.name}`))
                     .catch(error => console.error(`Failed to ban user ${userId} from server ${guild.name}`, error));
