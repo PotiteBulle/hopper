61d4fd5c861123c7e1467b9bb5116676175a0000
Potite_Bulle <135218431+bashx00@users.noreply.github.com>
2024-03-29 21:19:51 +0100

    Update + Inclusives code

--- hopper.js
+++ hopper.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-
-// Importer le token du bot et l'identifiant du serveur depuis le fichier de configuration
-const { token, serverId } = require('./security/config.json');
+const { token } = require('./security/config.json');
 
 // Créer une instance du client Discord
 const client = new Discord.Client();
@@ -11,29 +9,25 @@ const client = new Discord.Client();
 client.once('ready', () => {
     console.log('Bot is ready.');
 
-    // Récupérer le serveur Discord à partir de son identifiant
-    const guild = client.guilds.cache.get(serverId);
-    if (!guild) {
-        console.error('Server not found.');
-        return;
-    }
-
-    // Lire la liste des utilisateurices à bannir depuis le fichier txt
+    // Lire la liste des utilisateurs à bannir depuis le fichier txt
     fs.readFile('./bannissements/usersToBan.txt', 'utf8', (err, data) => {
         if (err) {
             console.error('Failed to read usersToBan.txt:', err);
             return;
         }
 
-        // Séparer les identifiants des utilisateurices en utilisant des sauts de ligne comme délimiteurs
+        // Séparer les identifiants des utilisateurs en utilisant des sauts de ligne comme délimiteurs
         const userIds = data.trim().split('\n');
 
-        // Parcourir chaque identifiant d'utilisateurices
-        userIds.forEach(userId => {
-            // Bannir l'utilisateurices du serveur Discord avec la raison spécifiée
-            guild.members.ban(userId.trim(), { reason: 'Demandes et participations aux partages de contenus interdits.' })
-                .then(user => console.log(`Banned user: ${user.username}`))
-                .catch(error => console.error(`Failed to ban user: ${userId}`, error));
+        // Parcourir chaque serveur Discord où le bot est présent
+        client.guilds.cache.forEach(guild => {
+            // Parcourir chaque identifiant d'utilisateur
+            userIds.forEach(userId => {
+                // Bannir l'utilisateur du serveur Discord avec la raison spécifiée
+                guild.members.ban(userId.trim(), { reason: 'Demandes et participations aux partages de contenus interdits.' })
+                    .then(user => console.log(`Banned user ${user.id} from server ${guild.name}`))
+                    .catch(error => console.error(`Failed to ban user ${userId} from server ${guild.name}`, error));
+            });
         });
     });
 });
